1d603de658b59e541f7d9e2577d767598ffbbd52
CubeDr <spaceship00@naver.com>
2025-01-17 17:09:07 +0900

    Add metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import 'hyuni-style';
 import styles from './page.module.css';
 import VideoToGifConverter from './VideoToGifConverter';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Convert GIF',
+  description: 'Convert video files to GIF files.',
+};
 
 export default function Home() {
   return (
